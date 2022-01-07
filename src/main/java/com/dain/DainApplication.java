32860326a17bbad787ed68731506e8076a7819e0
SeongsuHa <hss25350@naver.com>
2022-01-07 13:13:57 +0900

    Initial commit

---
+++ src/main/java/com/dain/DainApplication.java
@@ -0,0 +1,13 @@
+package com.dain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dain/DainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
